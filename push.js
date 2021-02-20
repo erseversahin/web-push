e30aae0e2dee9d5dd44155eeadbc2882b1c6e1d4
Şahin ERSEVER <ersversahint@gmail.com>
2021-02-21 02:21:25 +0300

    fix

--- push.js
+++ push.js
@@ -1,18 +1,24 @@
-const push = require('web-push')
-
+var push = require('web-push');
 
 let vapidKeys = {
-    publicKey: 'BJHgyS6j49iLiYTO2w5m6gZsXg6LN4mVGk-_09blFxtgTjjs2rUlSVCuGvnp8BHsLXF62bLga2rhaAeHYKWwL9I',
-    privateKey: 'fpbpbIZRyCYE8icRWF26tHg5y9o6jjEPENcL8JcCgZU'
-}
+        publicKey: 'BEEIWUefs1gdbxuwNanLuzgAcbOI5Z4ekuRoq9qZ6sulBntqGCp3duNJVZdfQnu05egU3i-6Bivuh1_xCwAWkl8',
+        privateKey: 'MkinUPCfSFzIfE62kJ6miaLQdhTHChcPzCxw-9-tHtk'
+    };
 
-push.setVapidDetails('mailto:test@stebilisim.com', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails(
+    'mailto:test@stebilisim.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/eh-rA_5tfgI:APA91bEAPVLh-9kf4dCvv0Z7l91tugh8xyOWCfi6tKvLe_62w-qX8OWPiWOSEFGM6tguWZcyJaTaSx6VO5A6Ex9ypFqbIYO_4BYWEQIlgfqESH9zKoyST9ILxvrRIachODf38lROGJsm","expirationTime":null,"keys":{"p256dh":"BCekClKOiOrYTE1gCO1gvs2afJjM9q-xyIMfwcFVL-FFBjtXdr1oJ8NRNqnqedKAfWuQZFsvVt1NbbaOEfSvRA0","auth":"h5GeeZw-uTAkTbw73IcegA"}};
+let sub = {
+    endpoint: "https://fcm.googleapis.com/fcm/send/dUMntjXgYIQ:APA91bGaEOzp2-bP2CWsYV6_0v6UI9Kv3kOIKXndTRytlfCvgvktnO3DCLLWX-D6opOdLmY8-KCvTOwAHXlthBc_eV8LfoOJjSZ0zC_vLJXs7DWY4-eXok0GQ2wxmGyNk6vsSzRCnOtb",
+    expirationTime: null,
+    keys: {
+        p256dh: "BN40QhRbJF0k1gSx-AGybZjYBfIZTEnhrJnEXnghUoP0OAtQAQRn1FvIqfMq70NrZ4D98OQmPY2iHLWOu9vLZWA",
+        auth: "Zx9u9r7hSzCOuC8SZbqNNQ"
+    }
+};
 
-push.sendNotification(sub, 'test mesajı')
-
-// let vapidKeys = push.generateVAPIDKeys();
-//
-//
-// console.log(vapidKeys);
+push.sendNotification(sub, 'test message');
+console.log(push.generateVAPIDKeys());
