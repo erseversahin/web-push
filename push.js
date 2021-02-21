52c8e669745570d98d295eff5724687f638404ec
Şahin ERSEVER <ersversahint@gmail.com>
2021-02-21 15:48:21 +0300

    fix

--- push.js
+++ push.js
@@ -14,3 +14,6 @@ push.setVapidDetails(
 let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dzSpcHIaWHQ:APA91bHyvln79mrktdYgNjXQi4OK3U4WZ1utF3bvrHM40X7EvDx4TMlfrVmnu-6AIU-MSOBhY_g0WsOHc3Fql2NzJnEUljeLvzTIWSxAZYaAc1u-2EZTK2gz6B7NNHu9jS1ouXQK-MhK","expirationTime":null,"keys":{"p256dh":"BNSAI4jaw2dlIcUN-JSQDM55VZxuIwAz5F_t1kJyv6dl8Pw5lsD-yhVFhxW4Ds-4nirTczoKgLQSfVpZMn4OiE4","auth":"D7ALHKLXgR8a5CKrB8_vTg"}};
 
 push.sendNotification(sub, 'test message');
+
+
+
