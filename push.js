88913d98c3d98c45b9f986f99e3bbd3693a8ef96
Şahin ERSEVER <ersversahint@gmail.com>
2021-02-21 03:25:29 +0300

    fix

--- push.js
+++ push.js
@@ -12,11 +12,11 @@ push.setVapidDetails(
 );
 
 let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/dzSpcHIaWHQ:APA91bHyvln79mrktdYgNjXQi4OK3U4WZ1utF3bvrHM40X7EvDx4TMlfrVmnu-6AIU-MSOBhY_g0WsOHc3Fql2NzJnEUljeLvzTIWSxAZYaAc1u-2EZTK2gz6B7NNHu9jS1ouXQK-MhK",
-    "expirationTime": null,
-    "keys": {
-        "p256dh": "BNSAI4jaw2dlIcUN-JSQDM55VZxuIwAz5F_t1kJyv6dl8Pw5lsD-yhVFhxW4Ds-4nirTczoKgLQSfVpZMn4OiE4",
-        "auth": "D7ALHKLXgR8a5CKrB8_vTg"
+    endpoint: "https://par02p.notify.windows.com/w/?token=BQYAAAA3L9cgy9Uac6BTOYHBJeR7UXlb8zv7XUUv3XJsxK13D1fmiXzKPD%2fWgn0jN%2brIbWYdAc7TtzWeAaNKS8GqDKL3IkW6oBuw%2f%2b4mgTYLqXZleiBs1aw9hXVRlF9ptuq0ivAME9bniGWCP8nCgSFJhYxFVurtJXBNPeII%2bExElo0e18a%2fK1TdIIu0oj%2b2BNLFVpiwP%2fgzMWVMfBjRkefsMeIikD9TZ1V3HEVlZ%2fxPZeTCAPICAIKUaMxyHn0jGUJRSjp7wtHBIxSKy9AZwuG1uLkYXM8uOZ4Dn%2b3EsOJVXEa4mCLzR8WNvl3cLaQrEG4%2faTssrhSxm5h7O2RbV5HcW69YMrPw9yTZ4AWOEoR0kwJzmw%3d%3d",
+    expirationTime: null,
+    keys: {
+        p256dh: "BCAYZCywbkhr7GFWubm063Y2yDKDLkTFxFJGfI1q1StyicldpdFSvhT4ilev1pMqU6YJfVcF24EnCJLzHuZx-xQ",
+        auth: "z4yue1uvBXwXhFrRtUMW7A"
     }
 };
 
--- sw.js
+++ sw.js
@@ -1,17 +1,14 @@
 self.addEventListener('push', function(e) {
     var options = {
         body: 'This notification was generated from a push!',
-        icon: 'images/example.png',
         vibrate: [100, 50, 100],
         data: {
             dateOfArrival: Date.now(),
             primaryKey: '2'
         },
         actions: [
-            {action: 'explore', title: 'Explore this new world',
-                icon: 'images/checkmark.png'},
-            {action: 'close', title: 'Close',
-                icon: 'images/xmark.png'},
+            {action: 'explore', title: 'Explore this new world'},
+            {action: 'close', title: 'Close'},
         ]
     };
     e.waitUntil(
